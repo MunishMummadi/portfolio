8948cd24aa52c67e6a755f30558cf784d4107ffb
Munish <moneymindedmunish1@gmail.com>
2025-06-11 14:39:54 -0500

    feat: updated resume link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ const ACTIONS: Array<NavigationItem> = [
 	},
 	{
 		type: NavigationItemType.LINK,
-		href: 'https://drive.google.com/file/d/1RC81r0sBt2yU4HqAiMHZhKGZuGm5pV9f/view?usp=sharing',
+		href: 'https://drive.google.com/file/d/16NtQux0RrejgTFU3TiZoNNZB4q1o1JUG/view?usp=sharing',
 		icon: <Icon className="mr-3" icon="feather:copy" />,
 		text: 'Resume',
 	},
